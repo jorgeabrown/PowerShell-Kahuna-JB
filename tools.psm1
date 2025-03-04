f8dda8a17ac0dacd1fa36d8270e80d179ed39644
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-04 11:28:24 -0800

    Adding Tools

---
+++ tools.psm1
@@ -0,0 +1,20 @@
+Function Get-CorpCompSysinfo {
+    [cmdletbinding()]
+    Param(
+        [string[]]$ComputerName
+        )
+    
+    ForEach($computer in $ComputerName) {
+    
+    $compsys = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $ComputerName
+    $Bios = Get-CimInstance -ClassName Win32_BIOS -ComputerName $ComputerName
+    $properties = [ordered] @{
+                 'Computername' = $computer;
+                 'BiosSerial'   = $bios.SerialNumber;
+                 'Manufacturer' = $compsys.Manufacturer;
+                 'Model'        = $compsys.Model;
+                   }
+            $outputobject = New-Object -TypeName psobject -Property $properties
+            Write-Output $outputobject
+                                        }
+                                  }
