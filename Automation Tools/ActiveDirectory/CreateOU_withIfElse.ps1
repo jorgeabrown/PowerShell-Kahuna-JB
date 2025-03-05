876f7b7156d69dfcb00cac42c9d0d78593a8e81b
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-05 13:17:45 -0800

    Added Group

--- Automation Tools/ActiveDirectory/CreateOU_withIfElse.ps1
+++ Automation Tools/ActiveDirectory/CreateOU_withIfElse.ps1
@@ -14,3 +14,19 @@ Write-Output "The OU $OUName has been created."
 
 ### Clean
 Remove-ADOrganizationalUnit -Identity $OUPath
+
+### Create the variables for the group and path
+$ADGroup = "London Users"
+$GroupPath = "CN=$ADGroup,$OUPath"
+
+### Check if the group already exists
+if (Get-ADGroup -filter {DistinguishedName -eq $GroupPath} -SearchBase $OUPath -ErrorAction SilentlyContinue) {
+    Write-Output "The group $ADGroup already exists"
+### If the group does not exist, create it    
+} else {
+    New-ADGroup -Name $ADGroup -Path $OUPath -GroupScope Global -GroupCategory Security
+    Write-Output "The group $ADGroup has been created."
+}
+
+### Clean
+Remove-ADGroup -Identity $GroupPath
