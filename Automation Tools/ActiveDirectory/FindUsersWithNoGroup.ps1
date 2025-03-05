b3f4df80cc11c75b77378d250e0676cf8278d9cd
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-05 14:24:59 -0800

    Hashes

--- Automation Tools/ActiveDirectory/FindUsersWithNoGroup.ps1
+++ Automation Tools/ActiveDirectory/FindUsersWithNoGroup.ps1
@@ -1,9 +1,11 @@
+### Set variables for the group, OU, domain, and users
 $GroupName = "Sales"
 $OUName = "Sales"
 $DomainDN = "DC=Adatum,DC=com"
 $OUPath = "OU=$OUName,$DomainDN"
 $users = Get-ADUser -Filter * -SearchBase $OUPath
 
+### Loop through users and add them to the group if they are not already a member
 foreach ($user in $users) {
     $isMember = Get-ADGroupMember -Identity $GroupName -Recursive | Where-Object { $_.distinguishedName -eq $user.distinguishedName }
 
