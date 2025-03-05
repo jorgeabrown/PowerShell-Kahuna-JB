37e96ca828ddee4097f7a368bbf171a487318ec9
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-05 14:08:09 -0800

    Added Explanations

--- Automation Tools/ActiveDirectory/MoveUsers_ToOU.ps1
+++ Automation Tools/ActiveDirectory/MoveUsers_ToOU.ps1
@@ -1,7 +1,9 @@
+### Create variables for users, OU, and Group
 $users = Get-ADuser -filter {City -eq "London"} -Properties city
 $OUPath = "OU=$OUName,$DomainDN"
 $GroupName = "London Users"
 
+### Loop through all users and move them to the OU and add them to the group
 foreach ($user in $users) {
     Move-ADObject -Identity $user.DistinguishedName -TargetPath $OUPath
     Add-ADGroupMember -Identity $GroupName -Members $user
