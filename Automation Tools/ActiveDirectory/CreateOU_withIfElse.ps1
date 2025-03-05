97e17fed2af721f4fdbab25d88b6fc248b87a3a8
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-05 11:08:42 -0800

    New ps1

---
+++ Automation Tools/ActiveDirectory/CreateOU_withIfElse.ps1
@@ -0,0 +1,13 @@
+### Create the variables
+$OUName = "London"
+$DomainDN = "DC=Adatum,DC=com"
+$OUPath = "OU=$OUName,$DomainDN"
+
+### Check if the OU already exists
+if (Get-ADOrganizationalUnit -Filter {DistinguishedName -eq $OUPath} -SearchBase $DomainDN  -ErrorAction SilentlyContinue) {
+    Write-Output "The OU $OUName already exists"
+} else { 
+### Create the OU
+New-ADOrganizationalUnit -Name $OUName -Path $DomainDN
+Write-Output "The OU $OUName has been created."
+}
