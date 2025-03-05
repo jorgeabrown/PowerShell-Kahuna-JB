64f8b6819a3cfa58103e803d167536f0906300df
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-05 14:47:41 -0600

    Add hashes

--- Automation Tools/ActiveDirectory/FindUsers_ThatDontLog90Days.ps1
+++ Automation Tools/ActiveDirectory/FindUsers_ThatDontLog90Days.ps1
@@ -1,1 +1,5 @@
-$age = (Get-Date).AddDays(-90);Get-ADUser -Filter {LastLogonTimeStamp -lt $age -and enabled -eq $true} -Properties LastLogonTimeStamp
+### Set $age variable to -90 days
+$age = (Get-Date).AddDays(-90)
+
+### Get all users that have not logged in for 90 days
+Get-ADUser -Filter {LastLogonTimeStamp -lt $age -and enabled -eq $true} -Properties LastLogonTimeStamp
