28788c0ffe9673b27f2006ddb13dc1e40e95022e
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-17 14:51:30 -0700

    Changed ADUsers

--- Project/CreateADUsers.ps1
+++ Project/CreateADUsers.ps1
@@ -8,7 +8,16 @@ try {
     $path = "OU=" + $user.Department + ",DC=Adatum,DC=com"
     $upn = $user.First + "@adatum.com"
     $display = $user.First + " " + $user.Last
-    New-ADUser -Name $display -SamAccountName $user.First -UserPrincipalName $upn -GivenName $user.First -Surname $user.Last -DisplayName $display -Path $path -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) -Enabled $true
+    New-ADUser -Name $display `
+               -SamAccountName $user.First `
+               -UserPrincipalName $upn `
+               -GivenName $user.First `
+               -Surname $user.Last `
+               -DisplayName $display `
+               -Path $path `
+               -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) `
+               -ChangePasswordAtLogon $true `
+               -Enabled $true
             Write-Host "User $display created successfully" -ForegroundColor Green
 } catch {
             Write-Host "Failed to create user $display" -ForegroundColor Red
