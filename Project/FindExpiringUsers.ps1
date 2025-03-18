2c133807b218a46c9bb8cc3b4973b0d3c4382751
jorgeabrown <jorge.a.brown@outlook.com>
2025-03-18 16:27:13 -0700

    FindExpiringUsersCommit

--- Project/FindExpiringUsers.ps1
+++ Project/FindExpiringUsers.ps1
@@ -1,11 +1,22 @@
-### 
+### Set users with an expiration date of 14 days from today so you can test the script and see the results.
 $salesusers = Get-ADUser -Filter * -SearchBase "OU=Sales,DC=Adatum,DC=Com" | Select-Object samAccountName
 
 foreach ($user in $salesusers) {
     Set-ADUser -Identity $user.samAccountName -AccountExpirationDate (Get-Date).AddDays(14) -PasswordNeverExpires $false -Verbose
 }
 
-###
+### Set variables for the current date, expiration date, and the users that are expiring within 14 days.
 $today = Get-Date
 $expirationDate = $today.AddDays(14)
-$expiringUsers = Get-ADUser -filter {AccountExpirationDate -le $expirationDate} -Properties AccountExpirationDate | Select-Object Name, UserprincipalName, AccountExpirationDate
+$expiringUsers = Get-ADUser -filter {AccountExpirationDate -le $expirationDate -and AccountExpirationDate -gt $today} -Properties AccountExpirationDate | Select-Object Name, UserprincipalName, AccountExpirationDate
+
+### Check if there are any expiring users and export the results to a CSV file. It will show a count of the expiring users and provide an email template.
+if ($expiringUsers.count -gt 0) {
+    Write-Host "There were $($expiringUsers.count) expiring users found. You can find the list in the ExpiringUsers.csv file." -ForegroundColor Red
+    $expiringUsers | Export-Csv -Path "C:\Reports\ExpiringUsers.csv" -NoTypeInformation
+    Write-Host "Results exported to C:\Reports" -ForegroundColor Magenta
+    Write-Host "Here is an email Template: ALCON, if you are on the TO line, your account is set to expire within 14 days. Please complete your cyber training
+    and contact your supervisor for further guidance." -ForegroundColor Yellow
+} else {
+    Write-Host "No expiring users found." -ForegroundColor Green
+}
